0f3b388205b881cca55e511f52f4987f10decce6
A17DT <129091377+A17DT@users.noreply.github.com>
2025-07-23 16:10:41 +0530

    Update login.js

--- login.js
+++ login.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', async (e) => {
     const password = passwordInput.value;
 
     try {
-        const response = await fetch('http://localhost:5000/signin', {
+        const response = await fetch('https://webd-final-jwdg.onrender.com/signin', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, email, password })
