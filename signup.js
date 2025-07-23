5b0ddc48bb8159b06acf4e2f48425051f5faa070
A17DT <129091377+A17DT@users.noreply.github.com>
2025-07-23 16:08:57 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', async (e) => {
     const password = passwordInput.value;
 
     try {
-        const response = await fetch('http://localhost:5000/signup', {
+        const response = await fetch('https://webd-final-jwdg.onrender.com/signup', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, email, password })
